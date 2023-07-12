97b0e41bc2c84de6db7b729f64cc153ee141db99
KaterinaEf <katerinaeftychiou@gmail.com>
2023-07-12 18:04:05 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+public class Main {
+    public static void main(String[] args) {
+        noReturn();
+        System.out.println(sum(1,2));
+    }
+
+    private static void noReturn() {
+        System.out.println("No return");
+    }
+
+    private static int sum(int a, int b) {
+        return a + b;
+    }
+}
